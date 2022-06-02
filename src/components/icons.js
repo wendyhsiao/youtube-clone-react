4de670bb33a8ef4452a0e4cb21139a5d25d6f974
wendy.hsiao <wendy19940331@gmail.com>
2022-06-03 00:58:10 +0800

    add icon

---
+++ src/components/icons.js
@@ -0,0 +1,22 @@
+import { ReactComponent as Like } from '../assets/icons/like.svg';
+import { ReactComponent as LikeFill } from '../assets/icons/like-fill.svg';
+import { ReactComponent as Dislike } from '../assets/icons/dislike.svg';
+import { ReactComponent as Share } from '../assets/icons/share.svg';
+import { ReactComponent as Save } from '../assets/icons/save.svg';
+import { ReactComponent as Report } from '../assets/icons/report.svg';
+import { ReactComponent as Down } from '../assets/icons/down.svg';
+import { ReactComponent as UpDown } from '../assets/icons/updown.svg';
+import { ReactComponent as More } from '../assets/icons/more.svg';
+
+export default {
+  Like,
+  LikeFill,
+  Dislike,
+  Share,
+  Save,
+  Report,
+  Down,
+  UpDown,
+  More
+};
+
